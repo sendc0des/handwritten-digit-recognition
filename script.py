79c5c1248c71186ad02d8b086905b36f4512c1c4
Arjun <arjunshandilya05@gmail.com>
2025-10-23 22:43:19 +0530

    fixed init of weights and biases

--- script.py
+++ script.py
@@ -12,8 +12,8 @@ g = torch.Generator().manual_seed(2147483647)
 class Linear:
     
     def __init__(self, fan_in, fan_out, bias=True):
-        self.weight = torch.randn((fan_in, fan_out), generator=g)
-        self.bias = torch.randn((fan_out), generator=g)
+        self.weight = torch.randn((fan_in, fan_out)) * (fan_in**-0.5)
+        self.bias = torch.zeros(fan_out) if bias else None
 
     def __call__(self, x):
         self.out = x @ self.weight
@@ -51,8 +51,8 @@ class Sequential:
 (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
 
 # scale the values and reshape the tensors to make them 2D
-X_train = X_train.astype("float32") / 255.0
-X_test = X_test.astype("float32") / 255.0
+X_train = X_train.astype('float32') / 255.0
+X_test = X_test.astype('float32')  / 255.0
 
 X_train = X_train.reshape(-1,28*28)
 X_test = X_test.reshape(-1,28*28)
@@ -63,11 +63,8 @@ pytorch_tensors = [torch.from_numpy(t) for t in numpy_arr]
 X_train, y_train, X_test, y_test = pytorch_tensors
 
 # split the dataset into train, eval and test sets
-num_samples = len(X_train)
-n = 0.8
-split_index = int(num_samples * n)
-
-Xtr, X_val, ytr, y_val = X_train[:split_index], X_train[split_index:], y_train[:split_index], y_train[split_index:]
+n = int(0.9 * len(X_train))
+Xtr, X_val, ytr, y_val = X_train[:n], X_train[n:], y_train[:n], y_train[n:]
 
 # define the model
 n_hidden = 128
@@ -79,12 +76,15 @@ model = Sequential([
     Linear(n_hidden, num_digits),
 ])
 
+with torch.no_grad():
+    model.layers[-1].weight *= 0.1
+
 parameters = model.parameters()
 for p in parameters:
     p.requires_grad = True
 
 # train the model on the train set
-max_steps = 100000
+max_steps = 10000
 batch_size = 32
 lossi = []
 
@@ -94,10 +94,12 @@ for i in range(max_steps):
     Xb, yb = Xtr[ix], ytr[ix]
 
     logits = model(Xb)
-    # counts = logits.exp()
-    # probs = counts / counts.sum(axis=1, keepdim=True)
-    # loss = -probs[torch.arange(batch_size), yb.long()].log().mean()
-    loss = torch.nn.functional.cross_entropy(logits, yb.long())
+    logits_maxes = logits.max(axis=1, keepdim=True).values
+    stable_logits = logits - logits_maxes
+    counts = logits.exp()
+    probs = counts / counts.sum(axis=1, keepdim=True)
+    loss = -probs[torch.arange(batch_size), yb.long()].log().mean()
+    # loss = torch.nn.functional.cross_entropy(logits, yb.long())
 
     # backward pass
     for p in parameters:
@@ -105,26 +107,26 @@ for i in range(max_steps):
     loss.backward()
 
     # update
-    lr = 0.1 if i<70000 else 0.01
+    lr = 0.1
     for p in parameters:
         p.data += -lr * p.grad
 
     # track stats
-    if i % 10000 == 0:
+    if i % 1000 == 0:
         print(f'{i:7d}/{max_steps:7d}: {loss.item():.4f}')
     lossi.append(loss.log10().item())
 
 # evaluate loss on train set and val set
-@torch.no_grad() # this decorator disables gradient tracking
-def split_loss(split):
-	x,y = {
-		'train': (Xtr, ytr),
-		'val': (X_val, y_val),
-		'test': (X_test, y_test),
-		}[split]
-	logits = model(x)
-	loss = torch.nn.functional.cross_entropy(logits, y)
-	print(split, loss.item())
+with torch.no_grad():
+    def split_loss(split):
+        x,y = {
+            'train': (Xtr, ytr),
+            'val': (X_val, y_val),
+            'test': (X_test, y_test),
+            }[split]
+        logits = model(x)
+        loss = torch.nn.functional.cross_entropy(logits, y)
+        print(split, loss.item())
 
 split_loss('train')
 split_loss('val')
